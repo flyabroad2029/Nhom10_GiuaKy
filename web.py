1c4693bd8a0626369903289581857ef99ccbf152
systemmanagementstoresport-droid <systemmanagementstoresport@gmail.com>
2026-01-29 09:26:28 +0700

    Cap nhat hien thi lua chon cua nguoi choi

--- web.py
+++ web.py
@@ -14,16 +14,36 @@ def send_choice(player, choice):
 @app.route("/player1", methods=["GET", "POST"])
 def player1():
     result = ""
+    player_choice = None
+
     if request.method == "POST":
-        result = send_choice("player1", request.form["choice"])
-    return render_template("play.html", player="Player 1", result=result)
+        player_choice = request.form["choice"]
+        result = send_choice("player1", player_choice)
+
+    return render_template(
+        "play.html",
+        player="Player 1",
+        result=result,
+        player_choice=player_choice
+    )
+
 
 @app.route("/player2", methods=["GET", "POST"])
 def player2():
     result = ""
+    player_choice = None
+
     if request.method == "POST":
-        result = send_choice("player2", request.form["choice"])
-    return render_template("play.html", player="Player 2", result=result)
+        player_choice = request.form["choice"]
+        result = send_choice("player2", player_choice)
+
+    return render_template(
+        "play.html",
+        player="Player 2",
+        result=result,
+        player_choice=player_choice
+    )
+
 
 if __name__ == "__main__":
     app.run()
